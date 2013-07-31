344000df9a322a28c942c19f23749dc9593e0f36
Michael Shavlovsky <mshavlovsky@gmail.com>
2013-07-31 12:25:28 -0700

    Tested vanilla Karger's algorithm is in.

--- graph_k.py
+++ graph_k.py
@@ -13,7 +13,6 @@ class Item(object):
         self.weight = 0
         # A list of messages from users.
         self.msgs = []
-        self.users = []
 
 
 class User(object):
@@ -35,7 +34,6 @@ class User(object):
         self.answers = {}
         # A list of messages from items.
         self.msgs = []
-        self.items = []
 
 
 class Msg(object):
@@ -49,13 +47,33 @@ class Msg(object):
 class Graph(object):
 
     def __init__(self):
-        self.users = users
-        self.items = items
+        self.users = []
+        self.items = []
         self.item_dict = {}
         self.user_dict = {}
         # normalization is to normalize user's reliability.
         self.normaliz = 1
 
+    def add_answer(self, user_id, item_id, answer):
+        """ For testing purposes"""
+        u = self.user_dict.get(user_id)
+        if not u:
+            u = User(user_id)
+            self.users.append(u)
+            self.user_dict[user_id] = u
+        it = self.item_dict.get(item_id)
+        if not it:
+            it = Item(item_id)
+            self.items.append(it)
+            self.item_dict[item_id] = it
+        # Adds answer
+        u.answers[it.id] = answer
+
+    def get_item(self, item_id):
+        return self.item_dict.get(item_id)
+
+    def get_user(self, user_id):
+        return self.user_dict.get(user_id)
 
     def _propagate_from_users(self):
         for it in self.items:
@@ -81,7 +99,7 @@ class Graph(object):
             for msg in u.msgs:
                 val = u.reliability - msg.value * u.answers[msg.source_id]
                 val /= self.normaliz
-                it = self.items_dict[msg.source_id]
+                it = self.item_dict[msg.source_id]
                 it.msgs.append(Msg(u.id, val))
             # Computes normalized reliability
             u.reliability /= self.normaliz
@@ -96,21 +114,38 @@ class Graph(object):
             # annotations in the same way we have base reliability.
             # todo(michael: Another point is that, after user's base reliability
             # reaches enough threshold, we want to cap it so it
-            # would not screw (make very small) other values after normalization.
+            # would not screw (make very small) other values after normalization
             it.weight = 0
             for msg in it.msgs:
-                u = self.users_dict[msg.source_id]
+                u = self.user_dict[msg.source_id]
                 it.weight += msg.value * u.answers[it.id]
             # Sends messages to users.
             for msg in it.msgs:
-                u = self.users_dict[msg.source_id]
+                u = self.user_dict[msg.source_id]
                 val = it.weight - msg.value * u.answers[it.id]
                 u.msgs.append(Msg(it.id, val))
+
+    def _aggregate_items(self):
+        """ Aggregates information for items """
+        for it in self.items:
+            it.weight = 0
+            for msg in it.msgs:
+                u = self.user_dict[msg.source_id]
+                it.weight += msg.value * u.answers[it.id]
 
 
     def run_full_computation(self, k_max):
         # Sends the initial messages from users to items.
         for it in self.items:
-            it.msg = []
-
-
+            it.msgs = []
+        for u in self.users:
+            for it_id in u.answers.iterkeys():
+                it = self.item_dict[it_id]
+                # todo(michael): is it necessary to randomize initial y's?
+                it.msgs.append(Msg(u.id, u.answers[it_id] * np.random.normal(1,1)))
+        # Runs main iterations.
+        for i in xrange(k_max):
+            self._propagate_from_items()
+            self._propagate_from_users()
+        # Aggregating item information.
+        self._aggregate_items()
---
+++ testKargerVanilla.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import unittest
+import graph_k as gk
+
+
+class TestKargerVanilla(unittest.TestCase):
+
+    def test_2_against_1(self):
+        # Creating users and items.
+        g = gk.Graph()
+        g.add_answer('u1', 'it1', 1)
+        g.add_answer('u1', 'it2', 1)
+        g.add_answer('u1', 'it3', -1)
+        g.add_answer('u2', 'it1', 1)
+        g.add_answer('u2', 'it2', 1)
+        g.add_answer('u2', 'it3', -1)
+        g.add_answer('u3', 'it1', -1)
+        g.add_answer('u3', 'it2', -1)
+        # Runs main algo
+        g.run_full_computation(10)
+        u1 = g.get_user('u1')
+        u2 = g.get_user('u2')
+        u3 = g.get_user('u3')
+        it1 = g.get_item('it1')
+        it2 = g.get_item('it2')
+        it3 = g.get_item('it3')
+
+        self.assertTrue(it1.weight > 0)
+        self.assertTrue(it2.weight > 0)
+        self.assertTrue(it3.weight < 0)
+        self.assertTrue(u1.reliability > 0)
+        self.assertTrue(u2.reliability > 0)
+        self.assertTrue(u3.reliability < 0)
+
+    def test_4_against_2(self):
+        # Creating users and items.
+        g = gk.Graph()
+        g.add_answer('u1', 'it1', 1)
+        g.add_answer('u1', 'it2', 1)
+        g.add_answer('u2', 'it1', 1)
+        g.add_answer('u2', 'it2', 1)
+        g.add_answer('u3', 'it1', 1)
+        g.add_answer('u3', 'it2', 1)
+        g.add_answer('u4', 'it1', 1)
+        g.add_answer('u4', 'it2', 1)
+        g.add_answer('u5', 'it1', -1)
+        g.add_answer('u5', 'it2', -1)
+        g.add_answer('u6', 'it1', -1)
+        g.add_answer('u6', 'it2', -1)
+        # User 7 is not consistend with anybody.
+        g.add_answer('u7', 'it1', -1)
+        g.add_answer('u7', 'it2', +1)
+        # Runs main algo
+        g.run_full_computation(100)
+        u1 = g.get_user('u1')
+        u5 = g.get_user('u5')
+        u7 = g.get_user('u7')
+        it1 = g.get_item('it1')
+        it2 = g.get_item('it2')
+
+        self.assertTrue(it1.weight > 0)
+        self.assertTrue(it2.weight > 0)
+        self.assertTrue(u1.reliability > 0)
+        self.assertTrue(u5.reliability < 0)
+
+if __name__ == '__main__':
+    unittest.main()
